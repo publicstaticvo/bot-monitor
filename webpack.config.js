e1bec3a1739799258a9da3b048e8f42ad7b0249d
publicstaticvo <42710459+publicstaticvo@users.noreply.github.com>
2019-10-05 16:00:45 +0800

    Create webpack.config.js
    0.3

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+var config = {
+	entry: {
+		main: './src/main',
+	},
+	output: {
+		path: path.join(__dirname,'/dist'),
+		publicPath: "/",
+		filename: "main.js",
+	},
+	module:{
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						css: ExtractTextPlugin.extract({
+							use: 'css-loader',
+							fallback: 'vue-style-loader',
+						}),
+					},
+				},
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					use: 'css-loader',
+					fallback: 'style-loader',
+				})
+			},
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin('main.css'),
+		new VueLoaderPlugin(),
+	],
+};
+module.exports=config;
